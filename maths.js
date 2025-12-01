bd3b1a4e09927208a4e450ad423550893c0da41d
Kiran Sai Panchakarla <panchakarlakiransai@gmail.com>
2025-12-01 19:09:20 +0530

    node js added

---
+++ maths.js
@@ -0,0 +1,8 @@
+console.log("JavaScript Math Object\n");
+
+console.log("Math.abs(-4.7):", Math.abs(-4.7));
+console.log("Math.ceil(4.4):", Math.ceil(4.4));
+console.log("Math.floor(4.7):", Math.floor(4.7));
+console.log("Math.sin(90 * Math.PI / 180):", Math.sin(90 * Math.PI / 180));
+console.log("Math.min(0, 150, 30, 20, -8, -200):", Math.min(0, 150, 30, 20, -8, -200));
+console.log("Math.random():", Math.random());
